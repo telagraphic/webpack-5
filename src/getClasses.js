f3a77d15f1171096d4134d55b4f822571a773a34
telagraphic <telagraphic@gmail.com>
2022-05-18 14:59:51 -0400

    basic setup for webpack 5

---
+++ src/getClasses.js
@@ -0,0 +1,10 @@
+import { wizard } from './wizard'
+import { hacker } from './hacker'
+
+function getClasses() {
+  console.log("get classes was called again")
+  console.log(wizard)
+  console.log(hacker)
+}
+
+export default getClasses
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import getClasses from './getClasses'
+
+getClasses()
+console.log("hawhoa")
+
+const obj = {a: 'alpha', b: "bravo"}
+const newObj = {...obj, c: "cravo"}
+console.log(newObj)
